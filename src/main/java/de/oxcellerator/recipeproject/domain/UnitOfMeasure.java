13cc1dea1f6c73ff618e4fa19b56c3f498b33d17
Olexiy Sokurenko <ol.sakura@gmail.com>
2018-02-16 12:12:58 +0100

    Domain classes added

---
+++ src/main/java/de/oxcellerator/recipeproject/domain/UnitOfMeasure.java
@@ -0,0 +1,42 @@
+package de.oxcellerator.recipeproject.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * @author <a:href="mailto:ol.sakura@gmail.com">Olexiy Sokurenko</a>
+ **/
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String uom;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUom() {
+        return uom;
+    }
+
+    public void setUom(String uom) {
+        this.uom = uom;
+    }
+
+    @Override
+    public String toString() {
+        return "UnitOfMeasure{" +
+                "id=" + id +
+                ", uom='" + uom + '\'' +
+                '}';
+    }
+}
