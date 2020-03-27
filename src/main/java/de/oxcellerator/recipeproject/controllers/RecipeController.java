9c3172a9fa3b6db22badc68da35ed995de6f1826
Olexiy Sokurenko <ol.sakura@gmail.com>
2020-03-27 17:06:41 +0100

    JaCoco code coverage and style fix

--- src/main/java/de/oxcellerator/recipeproject/controllers/RecipeController.java
+++ src/main/java/de/oxcellerator/recipeproject/controllers/RecipeController.java
@@ -3,8 +3,8 @@ package de.oxcellerator.recipeproject.controllers;
 import de.oxcellerator.recipeproject.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author Olexiy Sokurenko
@@ -18,7 +18,7 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/show/{id}")
+    @GetMapping("/recipe/show/{id}")
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(Long.parseLong(id)));
         return "recipe/show";
